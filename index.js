9a53b7e414c1fb08ff82c2213f705df2645ca5a9
Jesper Henrichsen <jesper.henrichsen@gmail.com>
2016-02-29 14:31:39 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,114 @@
+/*
+ *  Entry point for express-prerender 
+ *  mainly for setting up configurations
+ *
+ *  The `configs` object has the form:
+ *
+ *  {
+ *      cache_path  : path/to/cached_filed,
+ *      dist_folder : path/to/distribution folder
+ *  }
+ *
+ *  The `cached_path` is the folder where the cached files will be stored 
+ *  and served from.
+ *
+ *  `dist` is the path to the distribution folder of the website. 
+ *  The `dist` is used to see wether the cached files are still valid
+ *  using the `dist` last modified stamp. `dist` can also be a path to a
+ *  file if no folder is relevant.
+ *
+ * */
+var fs = require('fs'),
+    cache_page = "prerender/rendered/",
+    stringDecoder = require("string_decoder").StringDecoder,
+    decoder = new stringDecoder('utf8'),
+    cache_path = "",
+    dist_folder = "";
+
+/*
+    Module returns a prerendered html page
+    if the requests comes from a Robot
+*/
+var _prerender = function(_request, _response, _next) {
+    // remove any trailing slashes
+    var _fname = _request.charAt(_request.path.length - 1) == "/" ? _request.path.slice(0, _request.path.length - 1) : _request.path;
+    // replace remaining dashes with underscores
+    var _fname = _fname.replace(/[\/]/g, "_") + ".html";
+
+    var getCache = function() {
+        var path = _request.path,
+            cache = null,
+            dist = fs.lstatSync(dist_folder);
+        try {
+            cache = fs.lstatSync(cache_page + _fname);
+        } catch (e) {
+            console.log("Page %s is not cached ", path);
+        }
+        if (cache && cache.isFile()) {
+            var cacheAge = cache.mtime.getTime();
+            var buildAge = dist.mtime.getTime();
+            valid = cacheAge > buildAge;
+            console.log(_fname, " is already cached and valid: ", valid);
+            if (valid) {
+                var file = decoder.write(fs.readFileSync(cache_page + _fname));
+                _response.send(file);
+            } else {
+                console.log("Cached page ", _fname, " no longer valid");
+                getPage();
+            }
+        } else {
+            getPage();
+        }
+    }
+
+    // scrapes the page from the running page
+    var getPage = function() {
+        var spawn = require("child_process").spawn,
+            path = _request.path;
+        phantom = spawn("phantomjs", ["--ignore-ssl-errors=true", "prerender/scraper.js", path, cache_path]);
+        phantom.stdout.on("data", function(data) {
+            console.log("Output from PhantomJS:\n");
+            console.log(decoder.write(data));
+        });
+        phantom.stderr.on("data", function(data) {
+            console.log("PhantomJS ran into errors:\n");
+            console.log(decoder.write(data));
+        });
+        phantom.on("close", function(code) {
+            console.log("phantom closed with code ", code);
+            if (code === 0) {
+                var file = decoder.write(fs.readFileSync(cache_page + _fname));
+                _response.send(file);
+            } else {
+                // didnt get the page, best better
+                // continue as nomral
+                _next();
+            }
+        });
+    };
+
+    var readUseragent = function() {
+        if (_request.path.indexOf(".") >= 0) {
+            _next();
+        } else {
+            var useragent = require("express-useragent"),
+                source = _request.headers['user-agent'],
+                ua = useragent.parse(source);
+            if (ua.isBot && source.indexOf("PhantomJS") < 0) {
+                getCache();
+            } else {
+                _next()
+            }
+        }
+    }();
+}
+
+module.exports  = function(configs) {
+    cache_path  = configs.cache_path;
+    dist_folder = configs.dist_folder; 
+    
+    var prerender = _prerender;
+}
+
+
+
---
+++ scraper.js
@@ -0,0 +1,35 @@
+var page = require('webpage').create(),
+    fs = require('fs'),
+    host = "https://localhost",
+    system = require("system"),
+    args = system.args;
+
+console.log("Shipfeed scraper is running ");
+var statusCode = null;
+page.onResourceReceived = function(resource) {
+    statusCode = resource.status;
+};
+
+if (args.length === 3) {
+    var path = String(args[1]);
+    var fname = path.replace(/[\/]/g, "_") + ".html";
+    var cache_path = String(args[2]);
+} else {
+    // required param exit with some error: 4000
+    phantom.exit(4000);
+}
+
+page.clearMemoryCache();
+
+page.open(host + path, function() {
+    if (statusCode < 300 && statusCode >= 200) {
+        console.log("getting: ", host + path);
+        console.log("saving to: ", cache_path + fname);
+        fs.write(cache_path + fname, page.content, 'w');
+        phantom.exit(0);
+    } else {
+        // Bad response from server, exit
+        console.log("Bad status code: ", statusCode);
+        phantom.exit(4242);
+    }
+});
